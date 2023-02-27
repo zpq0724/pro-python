fb1402a248debaa4b8e518ac6c4b8607e0f70499
zhangpengqiang <zhangpengqiang@intra.nsfocus.com>
2023-02-27 17:48:15 +0800

    [feat] python 条件语句

--- Day1/003.条件语句.py
+++ Day1/003.条件语句.py
@@ -1,1 +1,14 @@
+# coding=utf-8
 # https://m.runoob.com/python/python-if-statement.html
+a = 1
+while a < 7:
+  if(a % 2 == 0):
+    print (a, "is even")
+  else:
+    print(a, 'is odd')
+  a += 1
+
+
+#for 循环语句
+for letter in 'Python':
+   print("当前字母:%s" % letter)
