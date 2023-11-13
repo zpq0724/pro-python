76e9c1fd3e81d48b879dd9a2dbc76194a8b5aceb
zpq0724 <zpq0724@163.com>
2023-11-13 08:15:30 +0800

    [feat] format 格式化函数

---
+++ Day1/数据类型.py
@@ -0,0 +1,8 @@
+'''
+  Python 中有五个标准的数据类型：
+    Numbers： int, long, float, complex
+    String
+    List (列表)
+    Tuple (元组): 类似List, 用() 标识，不能二次赋值，相当于只读列表。
+    Dictionary (字典)： 无序的对象集合
+'''
---
+++ Day4/test-format.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+name = "zpq"
+age = "18"
+#  3.6 及以上版本支持 f-string 来格式化字符串
+print(f"my name is {name}, I am {age} years old")
+
+print("my name is {}, I am {} years old" .format(name, age))
+
+
+
+'''
+  Python 中文编码
+  Python 中默认的编码格式是 ASCII 格式，没有修改编码格式时无法正确打印汉字
+  在文件开头加 # coding=utf-8
+  Python3.x 源码文件默认使用 uef-8 编码，所以可以正常解析中文，无需指定UTF-8编码
+'''
+print('hello world')
+
+print('你好，世界')
